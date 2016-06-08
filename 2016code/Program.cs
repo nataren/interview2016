f308c976c59dbecb6a0b31bb3f6abcaa4122122f
Cesar Lopez-Nataren <cesarn@mindtouch.com>
2016-06-08 10:06:10 -0700

    init import

---
+++ 2016code/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+
+public class HashTable<k, v> {
+    List<v> storeData= new List<v>();
+   
+    private int count = 0;
+    public HashTable() {
+    }
+
+    public void put(k key, v value) {
+        int hasCode = getHashCode(key);
+        storeData[hasCode] = value;
+
+    }
+
+    
+    public int getHashCode(k key) {
+        return count++;
+    }
+    public v get(k key) {
+
+
+    }
+}
+
